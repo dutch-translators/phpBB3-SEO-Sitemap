5ea07c0f222ca8610c9373ad3b68f6e3fe9a096b
David <neurofibromatose@live.nl>
2015-09-20 16:48:00 +0200

    De eerste lading
    Dit is een lastige, er zullen hier en daar nog wel wat verbeteringen doorgevoerd moeten worden.

--- nl/info_acp_seo_sitemap.php
+++ nl/info_acp_seo_sitemap.php
@@ -2,11 +2,12 @@
 
 /**
 *
-* info_acp_seo_sitemap [English]
+* info_acp_seo_sitemap [Dutch]
 *
 * @package phpBB3 SEO Sitemap
 * @copyright (c) 2014 www.phpbb-work.ru
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* Dutch translation by Dutch Translators (https://github.com/dutch-translators)
 *
 */
 
@@ -21,40 +22,40 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'SEOMAP'						=> 'SEO Sitemap',
-	'SEOMAP_VERSION'				=> 'Extension version: %s. See <a style="font-weight: bold;" href="https://www.phpbb.com/customise/db/extension/seo_sitemap/" onclick="window.open(this.href);return false;">extension page</a> for the latest version or to get help with this extension.',
+	'SEOMAP_VERSION'				=> 'Extensie versie: %s. See <a style="font-weight: bold;" href="https://www.phpbb.com/customise/db/extension/seo_sitemap/" onclick="window.open(this.href);return false;">extension page</a> for the latest version or to get help with this extension.',
 	'SEOMAP_EXPLAIN'				=> 'Pay attention that use of optional Priority and Changefreq settings could yield some positive results for you, but recommended using them together and really analysing which pages should be considered more important over others before making any decisions. If you do not know or are unsure, it is better to disable these options. <a href="http://www.sitemaps.org/protocol.html#xmlTagDefinitions" onclick="window.open(this.href);return false;">Click here</a> to read more details about priority and changefreq.<br />Enter 0 if you wish to disable priority.',
-	'SEOMAP_SETTINGS'				=> 'SEO Sitemap Settings',
-	'SEOMAP_SETTINGS_UPDATED'		=> '<strong>Updated SEO Sitemap Settings</strong>',
-	'SEOMAP_SAVED'					=> 'Sitemap Settings successfully updated.',
-	'SEOMAP_EXCLUDED'				=> 'Excluded forums',
-	'SEOMAP_EXCLUDED_EXPLAIN'		=> 'Forums selected here and topics inside them will be excluded from Sitemap.<br /><strong>Attention:</strong> Categories and forums without topics are excluded by default.',
+	'SEOMAP_SETTINGS'				=> 'SEO Sitemap Instellingen',
+	'SEOMAP_SETTINGS_UPDATED'		=> '<strong>SEO Sitemap Instellingen Bijgewerkt</strong>',
+	'SEOMAP_SAVED'					=> 'Sitemap Instellingen succesvol bijgewerkt.',
+	'SEOMAP_EXCLUDED'				=> 'Deze forums uitsluiten',
+	'SEOMAP_EXCLUDED_EXPLAIN'		=> 'De forumonderdelen(met alle onderwerpen die erin staan) die hier geselecteerd zijn, zullen niet worden weergegeven in de sitemap.<br /><strong>Let op:</strong> Categorieën en forumonderdelen zonder onderwerpen worden standaard uitgeslote.',
 	'SEOMAP_CACHE_TIME'				=> 'Cache time',
 	'SEOMAP_CACHE_TIME_EXPLAIN'		=> 'To reduce server load, sitemap will be cached for some time. After this time it will be re-created. Enter the number of hours for which sitemap will be cached or enter 0 for disable caching.',
 	'SEOMAP_URL'					=> 'Sitemap URL: <a href="%s" onclick="window.open(this.href);return false;">%s</a>',
-	'SEOMAP_URL_COUNT'				=> 'Total amount of URLs in sitemap: %s',
+	'SEOMAP_URL_COUNT'				=> 'Het aantal links in de sitemap: %s',
 	'SEOMAP_URL_LIMIT'				=> 'URL limit',
-	'SEOMAP_URL_LIMIT_EXPLAIN'		=> 'Maximum amount of URLs in sitemap file, up to 50000 according to sitemaps protocol. Decrease this value if you experience problems with exceeding limits. If the amount of URLs on your board exceeds this number, sitemap will automatically be split into multiple files.',
-	'SEOMAP_PRIORITY_0'				=> 'Priority for default topics',
-	'SEOMAP_PRIORITY_1'				=> 'Priority for sticky topics',
-	'SEOMAP_PRIORITY_2'				=> 'Priority for announcements',
-	'SEOMAP_PRIORITY_3'				=> 'Priority for global announcements',
-	'SEOMAP_PRIORITY_4'				=> 'Priority for article topics',
-	'SEOMAP_PRIORITY_F'				=> 'Priority for forums',
-	'SEOMAP_FREQ_0'					=> 'Changefreq for default topics',
-	'SEOMAP_FREQ_1'					=> 'Changefreq for sticky topics',
-	'SEOMAP_FREQ_2'					=> 'Changefreq for announcements',
-	'SEOMAP_FREQ_3'					=> 'Changefreq for global anouncements',
-	'SEOMAP_FREQ_4'					=> 'Changefreq for article topics',
-	'SEOMAP_FREQ_F'					=> 'Changefreq for forums',
-	'SEOMAP_FREQ_NEVER'				=> 'Never',
-	'SEOMAP_FREQ_YEARLY'			=> 'Yearly',
-	'SEOMAP_FREQ_MONTHLY'			=> 'Monthly',
-	'SEOMAP_FREQ_WEEKLY'			=> 'Weekly',
-	'SEOMAP_FREQ_DAILY'				=> 'Daily',
-	'SEOMAP_FREQ_HOURLY'			=> 'Hourly',
-	'SEOMAP_FREQ_ALWAYS'			=> 'Always',
-	'SEOMAP_NO_DATA'				=> 'No data for sitemap.',
-	'SEOMAP_CANT_WRITE'				=> 'Folder <strong>%s</strong> does not exist or is not writable. Fix it manually using FTP-client.',
+	'SEOMAP_URL_LIMIT_EXPLAIN'		=> 'Het maximum aantal links in je sitemap is 50000 volgens het sitemap protocol. Je kan deze waarde verlagen als je problemen ondervind met de ingestelde limiet. Als het aantal links van je forum deze waarde overschrijd, dan zal sitemap automatische worden verdeeld in meerdere bestanden.',
+	'SEOMAP_PRIORITY_0'				=> 'Prioriteit voor standaard onderwerpen',
+	'SEOMAP_PRIORITY_1'				=> 'Prioriteit voor sticky onderwerpen',
+	'SEOMAP_PRIORITY_2'				=> 'Prioriteit voor aankondigingen',
+	'SEOMAP_PRIORITY_3'				=> 'Prioriteit voor algemene aankondigingen',
+	'SEOMAP_PRIORITY_4'				=> 'Prioriteit voor article topics',
+	'SEOMAP_PRIORITY_F'				=> 'Prioriteit voor forums',
+	'SEOMAP_FREQ_0'					=> 'Veranderingsfrequentie voor standaard onderwerpen',
+	'SEOMAP_FREQ_1'					=> 'Veranderingsfrequentie voor sticky onderwerpen',
+	'SEOMAP_FREQ_2'					=> 'Veranderingsfrequentie voor aankondigingen',
+	'SEOMAP_FREQ_3'					=> 'Veranderingsfrequentie voor algemene aankondigingen',
+	'SEOMAP_FREQ_4'					=> 'Veranderingsfrequentie voor article topics',
+	'SEOMAP_FREQ_F'					=> 'Veranderingsfrequentie voor forums',
+	'SEOMAP_FREQ_NEVER'				=> 'Nooit',
+	'SEOMAP_FREQ_YEARLY'			=> 'Jaarlijks',
+	'SEOMAP_FREQ_MONTHLY'			=> 'Maandelijks',
+	'SEOMAP_FREQ_WEEKLY'			=> 'Wekelijks',
+	'SEOMAP_FREQ_DAILY'				=> 'Dagelijks',
+	'SEOMAP_FREQ_HOURLY'			=> 'Per uur',
+	'SEOMAP_FREQ_ALWAYS'			=> 'Altijd',
+	'SEOMAP_NO_DATA'				=> 'Geen gegevens voor sitemap.',
+	'SEOMAP_CANT_WRITE'				=> 'De map <strong>%s</strong> bestaat niet of is niet schrijfbaar. Je kan dit handmatig oplossen met behulp van je FTP-client.',
 
 // Sync section
 	'SEOMAP_SYNC_COMPLETE' 			=> 'Synchronisation successfully completed.<br /><br /><a style="font-weight: bold;" href="%s">&laquo; Go back to settings</a>',
